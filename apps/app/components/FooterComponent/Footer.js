42fa2ad092ac5756019bed56ae3b510d0a11da5d
umerfarooqlaghari <mumerfarooqlaghari@gmail.com>
2025-04-20 05:33:03 +0500

    Updated Footer

--- apps/app/components/FooterComponent/Footer.js
+++ apps/app/components/FooterComponent/Footer.js
@@ -5,25 +5,22 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="footer-content">
-        <div className="footer-col">
-          <h4>Contact Us</h4>
-          <p>Email: contact@baltariinc.com</p>
-          <p>Phone: +92 300 1234567</p>
-          <p>Address: Karachi, Pakistan</p>
+        {/* Contact Form */}
+        <div className="footer-left">
+          <h2 className="footer-heading">
+            Fill the form <span className="highlight">to contact us.</span>
+          </h2>
+          <form className="contact-form">
+            <div className="name-fields">
+              <input type="text" placeholder="First Name" />
+              <input type="text" placeholder="Last Name" />
+            </div>
+            <input type="email" placeholder="E-mail" />
+            <textarea placeholder="How can we help you? Describe your problem"></textarea>
+            <button type="submit">Send message</button>
+          </form>
         </div>
-        <div className="footer-col">
-          <h4>Quick Links</h4>
-          <p>Home</p>
-          <p>Services</p>
-          <p>Portfolio</p>
-          <p>Contact</p>
-        </div>
-        <div className="footer-col">
-          <h4>Stay in Touch</h4>
-          <p>Subscribe to our newsletter</p>
-          <input type="email" placeholder="Your email" />
-          <button>Subscribe</button>
-        </div>
+
       </div>
     </footer>
   )
