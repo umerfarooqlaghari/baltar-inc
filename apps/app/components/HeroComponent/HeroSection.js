27311c30e2107a07c486b6f96fc7ccb987b321f6
Umer Farooq <mumerfarooqlaghari@gmail.com>
2025-04-20 21:18:05 +0500

    Update HeroSection.js
    case sensitivity handled for vercel

--- apps/app/components/HeroComponent/HeroSection.js
+++ apps/app/components/HeroComponent/HeroSection.js
@@ -9,7 +9,7 @@ export default function HeroSection() {
         playsInline
         className="absolute top-0 left-0 w-full h-full object-cover z-0"
       >
-        <source src="/Video.mp4" type="video/mp4" />
+        <source src="/video.mp4" type="video/mp4" />
         Your browser does not support the video tag.
       </video>
 
